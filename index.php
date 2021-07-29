ccf447ded092f92576ac35051cdf678930f1d925
zejxmin <52437374+bigQY@users.noreply.github.com>
2021-07-29 19:49:44 +0800

    优化在手机端的展现

--- index.php
+++ index.php
@@ -22,6 +22,7 @@
 <html>
   <head>
     <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <title><?php echo get_title() . ' - ' . get_description(); ?></title>
     <meta name="description" content="" />
